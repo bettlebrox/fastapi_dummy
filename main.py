92798c338ea312e569c13715cd62316b99c2f787
David Burke <474346+bettlebrox@users.noreply.github.com>
2025-04-22 13:05:02 +0100

    fix: change msg

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ app.add_middleware(
 # Add root path handler
 @app.get("/")
 async def root():
-    return {"message": "Welcome to Audrey AI API dummy"}
+    return {"message": "Audrey AI API dummy"}
 
 
 # Add API routes
